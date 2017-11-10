206fa4bebb937a5e3142e7b5c76058984de2f937
guguobao <1690259624@qq.com>
2017-11-10 11:02:38 +0800

    the second updata

--- mainsite/views.py
+++ mainsite/views.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.template.loader import get_template
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
 # Create your views here.
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+#     post_lists = list()
+#     for count, post in enumerate(posts):
+#         post_lists.append("No.{}:".format(str(count))+str(post)+"<br>")
+    return HttpResponse(html)
